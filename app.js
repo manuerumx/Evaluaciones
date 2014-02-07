44b3649c2d02b17655c3992b57018b965a0ab7e3
Manuel González R <manuel@dsindigo.com>
2014-02-07 16:26:54 -0600

    404 Error page added

--- app.js
+++ app.js
@@ -30,6 +30,25 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//404 Errors
+app.use(function(req, res, next){
+	res.status(404);
+	// respond with html page
+	if (req.accepts('html')) {
+		res.render('404', { url: req.url });
+		return;
+	}
+	// respond with json
+	if (req.accepts('json')) {
+		res.send({ error: 'Not found' });
+		return;
+	}
+	// default to plain-text. send()
+	res.type('txt').send('Not found');
+});
+//404 Errors
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
